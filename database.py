b1b02c1c921f0c6230c7c831880bdf3109fe1ab8
Afonso Costa <afonso2004costa@gmail.com>
2025-01-30 15:07:02 +0000

    o url da database teve de ser alterado

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import sessionmaker # type: ignore
 from sqlalchemy import create_engine # type: ignore
 from sqlalchemy.ext.declarative import declarative_base # type: ignore
 
-DATABASE_URL = "mssql+pyodbc://databaseManagerAccount@damdbserv:,Gf5FVm6[0kUTA[8W)]D@damdbserv.database.windows.net:1433/damdb?driver=ODBC+Driver+17+for+SQL+Server"
+DATABASE_URL = "mssql+pyodbc://fonsi12@fonsi:Afonsinhogamerpt123%40@fonsi.database.windows.net:1433/dbfonsi?driver=ODBC+Driver+17+for+SQL+Server"
+
+
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
